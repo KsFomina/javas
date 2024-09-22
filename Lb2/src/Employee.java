77287a4fbed0cc53a95529ebf4567ff86f6b951d
KsFomina <sloveckija@gmail.com>
2024-09-22 22:14:20 +0300

    доделала 2

--- Lb2/src/Employee.java
+++ Lb2/src/Employee.java
@@ -1,22 +1,31 @@
 public class Employee {
-        private String name;
-        private int age;
-        private int experience;
+    private String name;
+    private int age;
+    private int experience;
 
-        public Employee(String n,int a,int e) {
-            name=n;
-            age=a;
-            experience=e;
-        }
+    public Employee(String n,int a,int e) {
+        name=n;
+        age=a;
+        experience=e;
+    }
 
-        @Override
-        public String toString() {
-            return "" +
-                    "name:" + name + "\n"+
-                    "age=" + age +"\n"+
-                    "experience=" + experience +"\n"+
-                    "___________________"+"\n";
-        }
+    @Override
+    public String toString() {
+        return "" +
+                "name:" + name + " age=" + age +" experience=" + experience +"\n";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Employee employee = (Employee) obj;
+        return age == employee.age && experience == employee.experience && name.equals(employee.name);
+    }
+    @Override
+    public int hashCode() {
+        return 31 * name.hashCode() + experience;
+    }
 
     public String getName() {
         return name;
--- Lb2/src/Main.java
+++ Lb2/src/Main.java
@@ -1,65 +1,88 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.HashSet;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void part1(){
+        //создание контейнеров типа Long
         ArrayList<Long> a = new ArrayList<>();
         ArrayList<Long> b = new ArrayList<>();
 
+        //заполнение контейнера данными
         a.add(9324L);
         a.add(124L);
         a.add(432L);
         a.add(64L);
         a.add(2L);
+        a.add(124L);
+        a.add(432L);
+        a.add(64L);
+        a.add(2L);
 
+        //Вывод контейнера
         System.out.println(a);
         System.out.println("---------------");
 
+        //удаление элемента по индексу
         a.remove(0);
         System.out.println(a);
         System.out.println("---------------");
 
+        //удаление элемента по значению
         a.remove(432L);
         System.out.println(a);
         System.out.println("---------------");
 
+        //Замена элементов
         for (int i = 0; i < a.size(); i++) {
             a.set(i,12L+i*2);
         }
-
         System.out.println(a);
         System.out.println("---------------");
 
+        //заполнение второго контейнера данными
         for (int i = 0; i < 5; i++) {
-            b.add(12L+i);
+            b.add(1L+i);
         }
 
+        //доступ к элементам с использованеим итераторов
         Iterator <Long> iter = a.iterator();
         while (iter.hasNext()){
             System.out.println(iter.next());
         }
+        System.out.println("---------------");
 
-        for (int i = 1; i < a.size();) {
-            a.remove(i);
+        //удаление нескольких элементов из первого контейнера
+        Long element = 14L; // Элемент после которого нужно удалить
+        int n = 5; // Количество элементов для удаления
+        int index=a.indexOf(element);
+        System.out.println(a);
+        for (int i = 0; i<n; i++) {
+            if (index<a.size()) a.remove(index);
         }
         System.out.println(b);
+        //Добавление элементов из второго контейнера в первый
         a.addAll(b);
         System.out.println(a);
 
     }
     public static void part2(){
+        //создание контейнеров типа Long
         ArrayList<Employee> a = new ArrayList<>();
         ArrayList<Employee> b = new ArrayList<>();
 
+        //заполнение контейнера данными
         a.add(new Employee("Вася",23,3));
         a.add(new Employee("Катя",20,0));
         a.add(new Employee("Сеня",20,1));
         a.add(new Employee("Кириллся",40,21));
         a.add(new Employee("Николай",33,10));
 
+        //вывод содержимого контейнера
         System.out.println(a);
         System.out.println("---------------");
 
@@ -71,9 +94,9 @@ public class Main {
         System.out.println(a);
         System.out.println("---------------");
 
-//        for (int i = 0; i < a.size(); i++) {
-//            a.set(i,12L+i*2);
-//        }
+        for (int i = 0; i < 2; i++) {
+            a.set(i,new Employee("Олег",20+i,i*3));
+        }
 
         System.out.println(a);
         System.out.println("---------------");
@@ -87,21 +110,69 @@ public class Main {
         while (iter.hasNext()){
             System.out.println(iter.next());
         }
+        System.out.println("---------------");
 
-        for (int i = 1; i < a.size();) {
-            a.remove(i);
+        //удаление нескольких элементов из первого контейнера
+        Employee element = new Employee("Кириллся", 40, 21); // Элемент после которого нужно удалить
+        int n = 2; // Количество элементов для удаления
+        int index = a.indexOf(element);
+
+        if (index != -1) { // Проверяем, найден ли элемент
+            // Удаляем n элементов начиная с index + 1
+            System.out.println(1);
+            for (int i = 0; i < n; i++) {
+                if (index < a.size()) {
+                    a.remove(index); // Удаляем следующий элемент
+                } else {
+                    break; // Если индекс выходит за пределы списка, выходим из цикла
+                }
+            }
         }
         System.out.println(b);
+        System.out.println("---------------");
         a.addAll(b);
         System.out.println(a);
 
     }
     public static void part3(){
         ArrayList<Employee> a = new ArrayList<>();
+        a.add(new Employee("Вася",23,3));
+        a.add(new Employee("Катя",20,0));
+        a.add(new Employee("Сеня",20,1));
+        a.add(new Employee("Кириллся",40,21));
+        a.add(new Employee("Николай",33,12));
         Comparator<Employee> comparator = Comparator.comparing(Employee::getAge);
         a.sort(comparator);
-        int i=0;
-        int aga=23;
+        System.out.println(a);
+
+        //создание второго контейнера типа TreeSet
+        Comparator<Employee> employeeComparator = Comparator
+                .comparing(Employee::getName)
+                .thenComparing(Employee::getAge);
+
+        TreeSet<Employee> employeeSet = new TreeSet<>(employeeComparator);
+
+        for (Employee employee : a) {
+            if (employee.getExperience() > 10) {
+                employeeSet.add(employee);
+            }
+        }
+        System.out.println(employeeSet);
+
+        Comparator<Employee> employeeComparator1 = Comparator
+                .comparing(Employee::getExperience);
+
+        TreeSet<Employee> employeeSet1 = new TreeSet<>(employeeComparator1.reversed());
+        employeeSet1.addAll(employeeSet);
+        System.out.println(employeeSet1);
+
+        Comparator<Employee> comparator1 = Comparator.comparing(Employee::getExperience);
+        a.sort(comparator1.reversed());
+        System.out.println(a);
+
+        HashSet<Employee> mergedSet = new HashSet<>(employeeSet1);
+        mergedSet.addAll(a);
+        System.out.println(mergedSet);
     }
     public static void main(String[] args) {
 //        part1();
